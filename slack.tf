80cd602d1df527d3070ad06dfba034e6ed875b3a
Yoz Grahame <ygrahame@launchdarkly.com>
2019-10-23 15:47:00 -0700

    Add slack project and flag

---
+++ slack.tf
@@ -0,0 +1,25 @@
+provider "launchdarkly" {
+}
+
+resource "launchdarkly_project" "slack" {
+  key  = "slack-app"
+  name = "Slack App"
+
+  tags = ["slack", "terraform-managed"]
+}
+
+resource "launchdarkly_feature_flag" "unfurl_links" {
+  project_key = launchdarkly_project.slack.key
+  name        = "Unfurl Links"
+  key         = "unfurl-links"
+  description = "whether or not to unfurl links"
+  maintainer_id = "ygrahame@launchdarkly.com"
+
+  variation_type = "boolean"
+  variations {
+    value = true
+  }
+  variations {
+    value = false
+  }
+}
